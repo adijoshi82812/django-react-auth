9b9f8cde01ee553df1d5ef1a2c379fb374311bef
= <=>
2021-06-13 18:57:20 +0530

    Added the nav bar
    This nav bar will render conditionally, if the user is logged in it will render the logged in nav and if the user is logged out it will render the logged out nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
+import Nav from './components/Nav';
+
 class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      logged_in: localStorage.getItem('token') ? true : false,
+    };
+  }
+
   render(){
     return(
       <div>
-        Django-react-auth
+        <Nav
+          logged_in={this.state.logged_in}
+        />
       </div>
     );
   }
---
+++ frontend/src/components/Nav.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class Nav extends Component{
+    render(){
+        const logged_out_nav = (
+            <nav
+                className="w3-bar w3-black w3-margin-bottom"
+            >
+                <button
+                    className="w3-bar-item w3-button w3-black"
+                >
+                    Login
+                </button>
+                <button
+                    className="w3-bar-item w3-button w3-black"
+                >
+                    Signup
+                </button>
+            </nav>
+        );
+
+        const logged_in_nav = (
+            <nav
+                className="w3-bar w3-black w3-margin-bottom"
+            >
+                <button
+                    className="w3-bar-item w3-button w3-black w3-right"
+                >
+                    Logout
+                </button>
+            </nav>
+        );
+
+        return this.props.logged_in ? logged_in_nav : logged_out_nav;
+    }
+}
+
+export default Nav;
